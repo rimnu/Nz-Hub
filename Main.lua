b4f91cffd6f605a88ae693a0a0364dc82a2abd2d
rimun <jiado5570@gmail.com>
2026-09-03 21:15:16 +0900

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+local SupportedGames = {
+    [114697347887839] = "https://raw.githubusercontent.com/rimnu/Main/refs/heads/main/%2B1%20Speed%20Monkey%20Escape.lua",
+    [137233438285284] = "https://raw.githubusercontent.com/rimnu/Main/refs/heads/main/Chicken%20Farm.lua",
+}
+
+local scriptUrl = SupportedGames[game.PlaceId]
+
+if scriptUrl then
+    loadstring(game:HttpGet(scriptUrl))()
+end
