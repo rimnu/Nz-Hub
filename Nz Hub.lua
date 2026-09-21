0f8a29baa0b073b17865203c442e721aaf785cb6
rimun <jiado5570@gmail.com>
2026-09-21 20:06:04 +0900

    Update Nz Hub.lua

--- Nz Hub.lua
+++ Nz Hub.lua
@@ -1,7 +1,7 @@
 local SupportedGames = {
-    [114697347887839] = "https://raw.githubusercontent.com/rimnu/Main/refs/heads/main/%2B1%20Speed%20Monkey%20Escape.lua",
-    [72858062353423] = "https://raw.githubusercontent.com/rimnu/Main/refs/heads/main/%2B1%20Speed%20Monkey%20Escape2.lua",
-    [137233438285284] = "https://raw.githubusercontent.com/rimnu/Main/refs/heads/main/Chicken%20Farm.lua",
+    [114697347887839] = "https://raw.githubusercontent.com/rimnu/Main1/refs/heads/main/%2B1%20Speed%20Monkey%20Escape.lua",
+    [72858062353423] = "https://raw.githubusercontent.com/rimnu/Main1/refs/heads/main/%2B1%20Speed%20Monkey%20Escape2.lua",
+    [137233438285284] = "https://raw.githubusercontent.com/rimnu/Main1/refs/heads/main/Chicken%20Farm.lua",
 }
 
 local scriptUrl = SupportedGames[game.PlaceId]
